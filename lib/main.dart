6f25df9deff6a605b337fbfca72a0dbffc24d5bf
Moustafa Eid <moustafameid@gmail.com>
2020-05-16 18:39:47 -0400

    Added Routes to add new pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:async';
+import 'package:printershare/pages/home.dart';
+
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => Home(),
+  },
 ));
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack (
-        children: <Widget>[
-          GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller){
-              _controller.complete(controller);
-            },
-          )
-        ],
-
-      )
-    );
-  }
-}
 
