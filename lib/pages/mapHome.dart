db16503e5d7aba0ce9564214eeb3220cf15ac812
Moustafa Eid <moustafameid@gmail.com>
2020-05-16 22:23:43 -0400

    Added Slideable Modal Container

--- lib/pages/mapHome.dart
+++ lib/pages/mapHome.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'package:expandable_bottom_sheet/expandable_bottom_sheet.dart';
+
 
 class MapHome extends StatefulWidget {
   @override
@@ -34,37 +36,59 @@ class _MapHomeState extends State<MapHome> {
 
   @override
   Widget build(BuildContext context) {
-    return _initialPosition == null
-        ? Container(
-      color: Colors.white,
-            child: Center(
-              child: Text(
-                'loading map..',
-                style: TextStyle(
-                    fontFamily: 'Avenir-Medium', color: Colors.grey[400]),
-              ),
-            ),
-          )
-        : Container(
-            child: SafeArea(
-              child: Stack(
-                children: <Widget>[
-                  GoogleMap(
-                    mapType: MapType.normal,
-                    initialCameraPosition: CameraPosition(
-                      target: _initialPosition,
-                      zoom: 14.4746,
-                    ),
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller.complete(controller);
-                    },
-                    zoomControlsEnabled: false,
-                    zoomGesturesEnabled: true,
-                    myLocationEnabled: true,
-                  )
-                ],
-              ),
-            ),
-          );
+    return ExpandableBottomSheet(
+      background:  _initialPosition == null
+          ? Container(
+        color: Colors.white,
+        child: Center(
+          child: Text(
+            'loading map..',
+            style: TextStyle(
+                fontFamily: 'Avenir-Medium', color: Colors.grey[400]),
+          ),
+        ),
+      )
+          : Container(
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: _initialPosition,
+                  zoom: 14.4746,
+                ),
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+                zoomControlsEnabled: false,
+                zoomGesturesEnabled: true,
+                myLocationEnabled: true,
+              )
+            ],
+          ),
+        ),
+      ),
+      persistentHeader: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+          color: Colors.blue,
+        ),
+        width: MediaQuery.of(context).size.width/1.1,
+        height: 40,
+        child: Center(
+          child: Text('Header'),
+        ),
+      ),
+      expandableContent: Container(
+        height: 500,
+        width: MediaQuery.of(context).size.width/1.1,
+        color: Colors.green,
+        child: Center(
+          child: Text('Content'),
+        ),
+      ),
+    );
   }
 }
